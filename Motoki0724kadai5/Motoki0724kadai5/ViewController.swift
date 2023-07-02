b67ee731b4a6da1940a7330a3263e4ec9393be1d
akio0911 <akio0911@gmail.com>
2023-07-02 19:27:34 +0900

    feedback

--- Motoki0724kadai5/Motoki0724kadai5/ViewController.swift
+++ Motoki0724kadai5/Motoki0724kadai5/ViewController.swift
@@ -25,47 +25,29 @@ class ViewController: UIViewController {
         //labelに割り算の結果を表示する
         label.text = String(culculatedResult)
         
-        //alertの表示内容を定義する
-        let title = "課題5"
-        let message1 = "割られる数を入力してください"
-        let message2 = "割る数を入力してください"
-        let message3 = "割る数には0を入力しないでください"
-        
-        //定数alertにUIAlertControllerを代入
-        let alert1 = UIAlertController(title: title, message: message1,  preferredStyle: .alert)
-            
-        let alert2 = UIAlertController(title: title, message: message2,  preferredStyle: .alert)
-            
-        let alert3 = UIAlertController(title: title, message: message3, preferredStyle: .alert)
-            
-        //OKボタンを作成
-        let ok = UIAlertAction(title: "OK", style: .default, handler: {action in print("OK")})
-        alert1.addAction(ok)
-        alert2.addAction(ok)
-        alert3.addAction(ok)
-        
         //TF1が空の時alert1を表示
         if textField1.text == "" {
-            present(alert1, animated: true, completion: nil)
+            presentAlert(message: "割られる数を入力してください")
+            return
         }
         
         //TF2が空の時alert2を表示
         if textField2.text == "" {
-            present(alert2, animated: true, completion: nil)
+            presentAlert(message: "割る数を入力してください")
+            return
         }
         
         //TF2に0が入力され計算された時、alert3を表示
         if value2 == 0 {
-            present(alert3, animated: true, completion: nil)
-            
-            }
+            presentAlert(message: "割る数には0を入力しないでください")
+            return
         }
     }
-    
 
-    
-    
-    
-    
-
-
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message,  preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+}
